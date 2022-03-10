9bdf24cf18002e88af9e8e7c84d92a271cb3a106
koenry <68077710+koenry@users.noreply.github.com>
2022-03-10 12:38:28 +0200

    Update run.py

--- run.py
+++ run.py
@@ -76,7 +76,7 @@ def pyData():
             return json.dumps(random.choice(arrayOfWrongChoices))
             
 if __name__ == '__main__':
-    app.run(host='0.0.0.0' debug = True)
+    app.run(host='0.0.0.0', debug = True)
    
 
 
