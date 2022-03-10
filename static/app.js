701f70f8ac25baafefc4d311497d2f44555f209b
koenry <sarunas.suchovas@gmail.com>
2022-03-10 12:06:53 +0200

    Changed JS variable for the server ip

--- static/app.js
+++ static/app.js
@@ -1,15 +1,15 @@
 document.addEventListener('keypress', function(event) {
   if (event.key === 'Enter' && (document.activeElement.tagName.toLowerCase() !== 'button')) {
-  
+    const serverName = '127.0.0.1'; // change to your server name or ip!
     let javascriptData = document.getElementById('textInput').value
-    fetch('http://127.0.0.1:5000/postmethod',  {
+    fetch(`http://${serverName}:5000/postmethod`,  {
       method: 'POST',
       body: JSON.stringify(javascriptData),
       headers: new Headers({
         'Content-Type': 'application/json'
       })
     })
-    fetch('http://127.0.0.1:5000/getpythondata')
+    fetch(`http://${serverName}:5000/getpythondata`)
       .then(res => res.json())
       .then(data => { 
     
